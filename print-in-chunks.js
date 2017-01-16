f5eef74078736133e3c538591897125674b36184
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-16 19:20:10 +0000

    Got the function to log data in chunks + newline

--- print-in-chunks.js
+++ print-in-chunks.js
@@ -1,3 +1,6 @@
+var https = require('https');
+
+
 function getAndPrintHTMLChunks () {
 
   var requestOptions = {
@@ -5,6 +8,20 @@ function getAndPrintHTMLChunks () {
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log('Chunk Received. ', data + '\n ');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream comple.')
+    })
+
+  });
 
 }
+
+getAndPrintHTMLChunks();
